b8e3e26397f4164bfb6855a58d03c99cbc9b8c46
meda997 <medatejaswi7@gmail.com>
2022-05-18 04:34:27 +0530

    [tejaswi];added code for reverse number

---
+++ LogicalPrograms/LogicalPrograms/ReverseANum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class ReverseANum
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int reverse;
+            int remainder;
+
+            Console.WriteLine("ENTER YOUR NUMBER:");
+            num = Convert.ToInt32(Console.ReadLine());
+            reverse = Convert.ToInt32(Console.ReadLine());
+
+            while (num == 0)
+            {
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num = num / 10;
+            }
+            {
+                Console.WriteLine("REVERSE OF NUMBER ");
+            }
+        }
+    }
+}
+
+
+
+
