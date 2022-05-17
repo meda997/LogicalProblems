0351ed8ee37744a499583f49714cac5ec115aa93
meda997 <medatejaswi7@gmail.com>
2022-05-18 03:51:49 +0530

    [tejaswi];added code for perfect number

---
+++ LogicalPrograms/LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    { 
+           public static void Main(string[] args)
+            {
+                int num;
+                int i = 1;
+                int total = 0;
+
+            Console.WriteLine("ENTER  NUMBER:");
+            num = Convert.ToInt32((double)i);
+                Console.ReadLine();
+
+                for (i = 1; i < num; i++)
+                {
+                    if (num % i == 0)
+                        total += i;
+                }
+                if (total == num)
+                    Console.Write("{0} is a perfect number", num);
+                else
+                    Console.Write("{0} is not a perfect number", num);
+            }
+        }
+    }
+
+
