8570229e4a3ff3af947e8fdd612c36732c3700c7
meda997 <medatejaswi7@gmail.com>
2022-05-18 03:40:53 +0530

    [tejaswi];added code for Fibonacci series

---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Fibonacci
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+                c = a + b;
+            }
+        }
+    }
+}
