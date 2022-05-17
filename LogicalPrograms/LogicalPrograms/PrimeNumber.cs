a32fe1b1644238a043e7ad9a341f8c4a9044a772
meda997 <medatejaswi7@gmail.com>
2022-05-18 04:09:45 +0530

    [tejaswi];added code for prime number

---
+++ LogicalPrograms/LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+
+        static void Main(string[] args)
+        { 
+                int num;
+                int i = 1;
+                int count = 0;
+            Console.WriteLine("ENTER YOUR NUMBER:");
+            num = Convert.ToInt32(Console.ReadLine());
+            
+
+            for (i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                        count++;
+                }
+
+                if (count == 2)
+                    Console.Write("{0} is a prime number", num);
+                else
+                    Console.Write("{0} is not a prime number", num);
+            }
+        }
+    }
+
